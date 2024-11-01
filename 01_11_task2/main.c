9e0a5560da65b73f39292d6f31a8af724fae8e1b
psiblvdegod <artyombaly@list.ru>
2024-11-01 16:31:19 +0300

    Update main.c

--- 01_11_task2/main.c
+++ 01_11_task2/main.c
@@ -10,26 +10,35 @@ bool validation(int array[], int arraySize) {
     }
     return true;
 }
-
-void swap(int *left, int * right) {
-    *left = *left + *right;
-    *right = *left - *right;
-    *left = *left - *right;
-}
-
-int * bogoSort(int array[], int arraySize) {
+ 
+void bogoSort(int array[], int arraySize) {
     while (!validation(array, arraySize)) {
         for (int i = 0; i < arraySize; ++i) {
-        swap(&(array[i]), &(array[rand() % arraySize]));
+            int temp = array[i];
+            int index = rand() % arraySize;
+            array[i] = array[index];
+            array[index] = temp;
         }
     }
-    return array;
+}
+
+bool bogoSortTest() {
+    int testArray[] = {1, 3, 4, 6, 9};
+    int testArraySize = sizeof(testArray) / sizeof(int);
+    bogoSort(testArray, testArraySize);
+    return validation(testArray, testArraySize);
 }
 
 int main(void) {
-    int array[] = {3, 2};
+    if (!bogoSortTest()) {
+        printf("Error. Test failed.\n");
+        return -1;
+    }
+    int array[] = {4, 2, 3, 1, 7, 4};
     int arraySize = (sizeof(array) / sizeof(int));
     bogoSort(array, arraySize);
-
+    for (int i = 0; i < arraySize; ++i) {
+        printf("%d ", array[i]);
+    }
     return 0;
 }
